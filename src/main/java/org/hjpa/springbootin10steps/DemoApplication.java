3629a66a5519e5f86e4a01f1ed59efde08566809
odinPcWin10 <odin.araujo.barragan@gmail.com>
2021-02-06 12:28:14 -0600

    writing a method to insert and update

--- src/main/java/org/hjpa/springbootin10steps/DemoApplication.java
+++ src/main/java/org/hjpa/springbootin10steps/DemoApplication.java
@@ -1,6 +1,5 @@
 package org.hjpa.springbootin10steps;
 
-
 import org.hjpa.springbootin10steps.entity.Course;
 import org.hjpa.springbootin10steps.repository.CourseRepository;
 import org.slf4j.Logger;
@@ -11,11 +10,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner{
-	private Logger log =LoggerFactory.getLogger(this.getClass());
-	
-	@Autowired CourseRepository repository;
-	
+public class DemoApplication implements CommandLineRunner {
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	CourseRepository repository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
@@ -24,8 +24,23 @@ public class DemoApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 //		Course course = repository.findById(1000L);
 //		log.info("\nCourse 1000 ----> "+course);
+
+//		repository.deleteById(1003L);
 		
-		repository.deleteById(1003L);
+		
+		/*Example save a Course*/
+		Course course = new Course("design pattern");
+		repository.save(course);
+		
+		
+		/*Example update a Course*/
+		Course course1001 = repository.findById(1001L);
+		course1001.setName("hibernate-jpa");
+		repository.save(course1001);
+		Course course1001upodated = repository.findById(1001L);
+		log.info("course1001 updated:\n"+course1001upodated);
+		
+		
 	}
 
 }
--- src/main/java/org/hjpa/springbootin10steps/repository/CourseRepository.java
+++ src/main/java/org/hjpa/springbootin10steps/repository/CourseRepository.java
@@ -17,10 +17,17 @@ public class CourseRepository {
 	public Course findById(Long id) {
 		return em.find(Course.class, id);
 	}
-//	public Course save(Course course)
 	public void deleteById(Long id) {
 		Course course = this.findById(id);
 		em.remove(course);
 	}
 
+	public Course save(Course course) {
+		if(course.getId() == null) {
+			em.persist(course);
+		}else {
+			em.merge(course);
+		}
+		return course;
+	}
 }
